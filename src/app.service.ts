1a17c804e68fae37df62476c68bffe077eb5d599
Md. Sultanul Arefin <mhmdarefin@gmail.com>
2024-09-06 19:46:10 +0600

    mongoose added

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+
+
+  get_logs =(message:string)=>{
+
+    console.log(message);
+
+
+    return;
+  }
+
+
+
   getHello(): string {
-    return 'Hello World!';
+
+    this.get_logs("Hello");
+    return 'Hello World! 2024';
   }
 }
